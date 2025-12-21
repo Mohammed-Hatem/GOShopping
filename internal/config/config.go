7fb3a88d28bc125ad89981f63b5c57fd33d14c86
Mohammed-Hatem <mr.mohammedhatem@gmail.com>
2025-12-21 12:04:26 +0200

    Added config file to load env variables

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+
+func NewConfig() (error) {
+	// Load environment variables from .env file
+	envPaths := []string{
+		".env",                      // Current directory
+		"../.env",                   // One level up
+		"../../.env",                // Two levels up (from cmd/api/)
+		filepath.Join("..", ".env"), // Alternative path format
+	}
+
+	var envLoaded bool
+	for _, envPath := range envPaths {
+		if err := godotenv.Load(envPath); err == nil {
+			envLoaded = true
+			break
+		}
+	}
+
+	if !envLoaded {
+		return fmt.Errorf("Canoot load the .env file")
+	}
+
+
+
+
+	return nil
+}
